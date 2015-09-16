78b2591cfa48cd982e528edad80cfaced4659dc8
Alternativo8 <Alternativo8@Alter8>
2015-09-16 15:21:04 -0300

    concluido set e map de fornecedor

--- src/com/fafica/atividade1/fornecedor/RepositorioFornecedorMap.java
+++ src/com/fafica/atividade1/fornecedor/RepositorioFornecedorMap.java
@@ -1,46 +1,84 @@
 package com.fafica.atividade1.fornecedor;
 
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
 
 import com.fafica.atividade.erros.FornecedorJaCadastradoException;
 import com.fafica.atividade.erros.FornecedorNaoEncontradoException;
 
-public class RepositorioFornecedorMap implements IRepositorioFornecedor{
+public class RepositorioFornecedorMap implements IRepositorioFornecedor {
+
+	Map<Integer, Fornecedor> mapFornecedor;
+	Integer index;
+
+	public RepositorioFornecedorMap() {
+		mapFornecedor = new TreeMap<Integer, Fornecedor>();
+		index = 1;
+
+	}
 
 	@Override
 	public void cadastrar(Fornecedor fornecedor) throws FornecedorJaCadastradoException {
-		// TODO Auto-generated method stub
-		
+		fornecedor.setCodigo(index);
+		mapFornecedor.put(index, fornecedor);
+		index++;
+
 	}
 
 	@Override
 	public void atualizar(Fornecedor fornecedor) {
-		// TODO Auto-generated method stub
-		
+		Integer i = fornecedor.getCodigo();
+		mapFornecedor.put(i, fornecedor);
+
 	}
 
 	@Override
 	public Boolean remover(String cpf) {
-		// TODO Auto-generated method stub
-		return null;
+		for (int i = 1; i < mapFornecedor.size(); i++) {
+			Fornecedor fornecedor = mapFornecedor.get(i);
+			if (cpf.equals(fornecedor.getCpf())) {
+				mapFornecedor.remove(fornecedor);
+				return true;
+			}
+		}
+		return false;
 	}
 
 	@Override
 	public Fornecedor procurar(String cpf) throws FornecedorNaoEncontradoException {
+		for (int i = 1; i < mapFornecedor.size(); i++) {
+			Fornecedor fornecedor = mapFornecedor.get(i);
+			if (cpf.equals(fornecedor.getCpf())) {
+				return fornecedor;
+			}else{
+				throw new FornecedorNaoEncontradoException();
+			}
+		}
 		return null;
-		
+
 	}
 
 	@Override
 	public Boolean existe(String cpf) {
-		// TODO Auto-generated method stub
-		return null;
+		for (int i = 1; i < mapFornecedor.size(); i++) {
+			Fornecedor fornecedor = mapFornecedor.get(i);
+			if (cpf.equals(fornecedor.getCpf())) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	@Override
 	public ArrayList<Fornecedor> listar() {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Fornecedor> arrayListfornecedorLocal = new ArrayList<Fornecedor>();
+		for (int i = 1; i < mapFornecedor.size(); i++) {
+			Fornecedor fornecedor = mapFornecedor.get(i);
+			arrayListfornecedorLocal.add(fornecedor);
+			
+		}
+		return arrayListfornecedorLocal;
 	}
 
 }
--- src/com/fafica/atividade1/fornecedor/RepositorioFornecedorSet.java
+++ src/com/fafica/atividade1/fornecedor/RepositorioFornecedorSet.java
@@ -1,47 +1,80 @@
 package com.fafica.atividade1.fornecedor;
 
 import java.util.ArrayList;
-
+import java.util.TreeSet;
 import com.fafica.atividade.erros.FornecedorJaCadastradoException;
 import com.fafica.atividade.erros.FornecedorNaoEncontradoException;
 
 public class RepositorioFornecedorSet implements IRepositorioFornecedor {
 
+	TreeSet<Fornecedor> arraySetFornecedor;
+	int index;
+
+	public RepositorioFornecedorSet() {
+		arraySetFornecedor = new TreeSet<Fornecedor>();
+		index = 1;
+		index++;
+	}
+
 	@Override
 	public void cadastrar(Fornecedor fornecedor) throws FornecedorJaCadastradoException {
-		// TODO Auto-generated method stub
-		
+		fornecedor.setCodigo(index);
+		arraySetFornecedor.add(fornecedor);
 	}
 
 	@Override
 	public void atualizar(Fornecedor fornecedor) {
-		// TODO Auto-generated method stub
-		
+		int i = fornecedor.getCodigo();
+		for (Fornecedor fornecedor2 : arraySetFornecedor) {
+			if (i == fornecedor2.getCodigo()) {
+				arraySetFornecedor.remove(fornecedor2);
+			}
+		}
+		arraySetFornecedor.add(fornecedor);
 	}
 
 	@Override
 	public Boolean remover(String cpf) {
-		// TODO Auto-generated method stub
-		return null;
+		for (Fornecedor fornecedor : arraySetFornecedor) {
+			if (cpf.equals(fornecedor.getCpf())) {
+				arraySetFornecedor.remove(fornecedor);
+				return true;
+			}
+		}
+		return false;
 	}
 
 	@Override
 	public Fornecedor procurar(String cpf) throws FornecedorNaoEncontradoException {
+		for (Fornecedor fornecedor : arraySetFornecedor) {
+			if(cpf.equals(fornecedor.getCpf())){
+				return fornecedor;
+			}else{
+				throw new FornecedorNaoEncontradoException();
+			}
+		}
 		return null;
-		
+
 	}
 
 	@Override
 	public Boolean existe(String cpf) {
-		// TODO Auto-generated method stub
-		return null;
+		for (Fornecedor fornecedor : arraySetFornecedor) {
+			if(cpf.equals(fornecedor.getCpf())){
+				return true;
+			}
+		}
+		return false;
 	}
 
 	@Override
 	public ArrayList<Fornecedor> listar() {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Fornecedor> arrayListfornecedorLocal = new ArrayList<Fornecedor>();
+		for (Fornecedor fornecedor : arraySetFornecedor) {
+			arrayListfornecedorLocal.add(fornecedor);
+		}
+		
+		return arrayListfornecedorLocal;
 	}
-	
 
 }
